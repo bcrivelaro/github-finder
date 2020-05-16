499f97f95e475dccc9d6882db0da211494426f2c
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2020-05-16 19:16:32 -0300

    Passing param UP

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import './App.css';
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/Search';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/search/users',
+      {
+        params: {
+          q: text,
+          client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+          client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET
+        }
+      }
+    )
+
+    this.setState({ users: res.data.items, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,33 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://www.github.com/mojombo'
-      },
-      {
-        id: '2',
-        login: 'bcrivelaro',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://www.github.com/bcrivelaro'
-      },
-      {
-        id: '3',
-        login: 'eliasousa',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://www.github.com/eliasousa'
-      },
-    ]
-  }
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => <UserItem key={user.id} user={user} />)}
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
       </div>
     )
   }
@@ -37,6 +21,11 @@ const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem'
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 export default Users
