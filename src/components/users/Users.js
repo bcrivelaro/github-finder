83bf1c27e5979b01a19f76fc8668419a4728d9f4
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2020-05-16 18:32:21 -0300

    Class based components

---
+++ src/components/users/Users.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import UserItem from './UserItem'
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: '1',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://www.github.com/mojombo'
+      },
+      {
+        id: '2',
+        login: 'bcrivelaro',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+        html_url: 'https://www.github.com/bcrivelaro'
+      },
+      {
+        id: '3',
+        login: 'eliasousa',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+        html_url: 'https://www.github.com/eliasousa'
+      },
+    ]
+  }
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => <UserItem key={user.id} user={user} />)}
+      </div>
+    )
+  }
+}
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+}
+
+export default Users
