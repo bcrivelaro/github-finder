1f482849c178d4fcde6fc98e5242410aac471b0f
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2020-05-17 11:17:52 -0300

    Change GH env vars

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -26,8 +26,8 @@ const GithubState = (props) => {
     const res = await axios.get('https://api.github.com/search/users', {
       params: {
         q: text,
-        client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
-        client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
       },
     });
 
